b85ee4bac7966c501e5fa3b6c15b0c637cf3a274
kenlallemand <kenlallemand@gmail.com>
2023-05-03 12:24:30 -0500

    stopwatch logic added

---
+++ assets/Palette.ts
@@ -0,0 +1,8 @@
+export const DARK_PRIMARY_COLOR = "#303F9F";
+export const LIGHT_PRIMARY_COLOR = "#C5CAE9";
+export const PRIMARY_COLOR = "#3F51B5";
+export const TEXT_OR_ICONS = "#FFFFFF";
+export const ACCENT_COLOR = "#03A9F4";
+export const PRIMARY_TEXT = "#212121";
+export const SECONDARY_TEXT = "#757575";
+export const DIVIDER_COLOR = "#BDBDBD";
---
+++ utils/StopWatch.ts
@@ -0,0 +1,17 @@
+import { Time } from "./TimeType";
+
+export function DisplayTimer({ milliseconds, seconds, minutes, hours }: Time) {
+	milliseconds += milliseconds;
+	if (milliseconds === 1000) {
+		seconds += 1;
+		milliseconds = 0;
+		if (seconds === 60) {
+			minutes += 1;
+			seconds = 0;
+			if (minutes === 60) {
+				hours += 1;
+				minutes = 0;
+			}
+		}
+	}
+}
